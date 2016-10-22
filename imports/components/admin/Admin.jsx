4226ae34a782cc35a0b29dada8f6cb6b406a6137
Afzal Hossain <afzal.csedu@gmail.com>
2016-10-22 19:01:42 +0600

    favicon + admin sidename

--- imports/components/admin/Admin.jsx
+++ imports/components/admin/Admin.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import Header from './layout/Header';
+import SideNav from './layout/SideNav';
 
 class Admin extends React.Component {
     render() {
         return (
             <div>
                 <Header />
-                <div className="admin container">
-                    <div className="row">
-                        <div className="col s12 l12">
-                            <h3>Admin Buttons Here</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum doloremque eligendi eveniet quidem. Assumenda excepturi doloremque, quisquam, dolor magnam ratione.</p>
-                        </div>
+                <div id="main">
+                    <div className="wrapper">
+                        <SideNav />
+                        <section id="content">
+                            <div className="admin container">
+                                <div className="row">
+                                    <div className="col s12 l12">
+                                        <h3>Admin Buttons Here</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum doloremque eligendi eveniet quidem. Assumenda excepturi doloremque, quisquam, dolor magnam ratione.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </div>
+
             </div>
         );
     }
---
+++ imports/components/admin/layout/SideNav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router';
+export default class SideNav extends React.Component {
+    componentDidMount() {
+        jQuery('.sidebar-collapse').sideNav({
+            edge: 'left' 
+        });
+    }
+    render() {
+        return (
+            <aside id="left-sidebar-nav">
+                <ul id="slide-out" className="side-nav fixed leftside-navigation">
+                    <li className="bold active">
+                        <Link to="admin" className="waves-effect waves-red">
+                            <i className="material-icons">dashboard</i> Dashboard</Link>
+                        <Link to="account" className="waves-effect waves-red">
+                            <i className="material-icons">menu</i> Account</Link>
+                    </li>
+                </ul>
+                <a href="#" data-activates="slide-out"
+                    className="sidebar-collapse btn-floating btn-medium waves-effect waves-light hide-on-large-only red">
+                    <i className="material-icons">menu</i>
+                </a>
+            </aside>
+        );
+    }
+}
