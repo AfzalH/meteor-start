f03899f2386904b439e6f7dfb89c52447ffc242f
Afzal Hossain <afzal.csedu@gmail.com>
2016-10-25 11:18:33 +0600

    change admin state to shallow object

--- imports/components/admin/Admin.jsx
+++ imports/components/admin/Admin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+var _ = require('lodash');
 
 import Header from './layout/Header';
 import SideNav from './layout/SideNav';
@@ -9,21 +10,13 @@ class Admin extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: {
-                perPage: 10,
-                filterText: '',
-                page: 1
-            }
+            users_per_page: 10,
+            users_filter_text: '',
+            users_page_no: 1
         }
     }
-    changeUsersState(key, value) {
-        var obj = this.state.users;
-        obj[key] = value;
-        this.setState(
-            {
-                users: obj
-            }
-        );
+    changeUsersPerPage(value) {
+        this.setState({ users_per_page: value });
     }
     render() {
         return (
@@ -38,7 +31,7 @@ class Admin extends React.Component {
                                     {React.cloneElement(this.props.children, {
                                         key: this.props.location.pathname,
                                         state: this.state,
-                                        changeUsersState: this.changeUsersState.bind(this)
+                                        changeUsersPerPage: this.changeUsersPerPage.bind(this)
                                     })}
                                 </ReactCSSTransitionGroup>
                             </div>
--- imports/components/admin/Users.jsx
+++ imports/components/admin/Users.jsx
@@ -9,18 +9,18 @@ class Users extends React.Component {
     }
     changePaginationValue(val) {
         // this.props.rootChangePaginationValue(val);
-        this.props.changeUsersState('perPage', val);
+        this.props.changeUsersPerPage(val);
     }
     render() {
-        let {perPage, filterText, page} = this.props.state.users;
+        let {users_per_page, users_filter_text, users_page_no} = this.props.state;
         return (
             <div className="row">
                 <div className="col s12 l12">
                     <h4>Lets administer our Users</h4>
-                    {(!this.props.usersReady) ? 
-                    <div className="progress">
-                        <div className="indeterminate"></div>
-                    </div> : <div className="progressPlaceholder"></div>}
+                    {(!this.props.usersReady) ?
+                        <div className="progress">
+                            <div className="indeterminate"></div>
+                        </div> : <div className="progressPlaceholder"></div>}
                     <ul className="collection with-header">
                         <li className="collection-header">
                             <div className="row">
@@ -28,14 +28,14 @@ class Users extends React.Component {
                                     <p>Filter</p>
                                 </div>
                                 <div className="col s4">
-                                    <a className="btn dropdown-button waves-effect waves-light right red lighten-1" data-activates="pagination-dropdown">{perPage} per page</a>
+                                    <a className="btn dropdown-button waves-effect waves-light right red lighten-1" data-activates="pagination-dropdown">{users_per_page} per page</a>
                                     <ul id="pagination-dropdown" className="dropdown-content">
-                                        {(perPage != 2) ? <li><a onClick={this.changePaginationValue.bind(this, 2)}>2</a></li> : ''}
-                                        {(perPage != 5) ? <li><a onClick={this.changePaginationValue.bind(this, 5)}>5</a></li> : ''}
-                                        {(perPage != 10) ? <li><a onClick={this.changePaginationValue.bind(this, 10)}>10</a></li> : ''}
-                                        {(perPage != 25) ? <li><a onClick={this.changePaginationValue.bind(this, 25)}>25</a></li> : ''}
-                                        {(perPage != 50) ? <li><a onClick={this.changePaginationValue.bind(this, 50)}>50</a></li> : ''}
-                                        {(perPage != 100) ? <li><a onClick={this.changePaginationValue.bind(this, 100)}>100</a></li> : ''}
+                                        {(users_per_page != 2) ? <li><a onClick={this.changePaginationValue.bind(this, 2)}>2</a></li> : ''}
+                                        {(users_per_page != 5) ? <li><a onClick={this.changePaginationValue.bind(this, 5)}>5</a></li> : ''}
+                                        {(users_per_page != 10) ? <li><a onClick={this.changePaginationValue.bind(this, 10)}>10</a></li> : ''}
+                                        {(users_per_page != 25) ? <li><a onClick={this.changePaginationValue.bind(this, 25)}>25</a></li> : ''}
+                                        {(users_per_page != 50) ? <li><a onClick={this.changePaginationValue.bind(this, 50)}>50</a></li> : ''}
+                                        {(users_per_page != 100) ? <li><a onClick={this.changePaginationValue.bind(this, 100)}>100</a></li> : ''}
                                     </ul>
                                 </div>
                             </div>
@@ -51,7 +51,7 @@ class Users extends React.Component {
 }
 
 export default createContainer((params) => {
-    let handle = Meteor.subscribe('users', params.state.users.perPage);
+    let handle = Meteor.subscribe('users', params.state.users_per_page);
     const users = Meteor.users.find({ _id: { $ne: Meteor.userId() } }).fetch();
-    return { users: users, state: params.state, changeUsersState: params.changeUsersState, usersReady: handle.ready() };
+    return { users: users, state: params.state, changeUsersPerPage: params.changeUsersPerPage, usersReady: handle.ready() };
 }, Users);
